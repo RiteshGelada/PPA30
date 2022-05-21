b856847e6a20287c516d4afe848ff33d28892740
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2022-05-21 08:33:16 +0530

    Update Exception1.java

--- Exception1.java
+++ Exception1.java
@@ -9,7 +9,7 @@ public class Exception1
        System.out.println("lease enter first number");
        int no1 = sobj.nextInt();
 
-       System.out.println("lease enter Second number");
+       System.out.println("lease enter Second number");    //
        int no2 = sobj.nextInt();
 
        int ans = no1/no2;
